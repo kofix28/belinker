1242e10305716d3117c0d89d52a9d21d63be39d9
kofix28 <sayejfadi2004@gmail.com>
2025-09-23 17:01:11 +0200

    bottom bar

--- lib/Bottom_bar.dart
+++ lib/Bottom_bar.dart
@@ -1,45 +1,38 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
+class BottomBar extends StatefulWidget {
+  final ValueChanged<int> onItemSelected; // 👈 callback to parent
 
-class BottomBar extends StatelessWidget {
-   const BottomBar({super.key});
+  const BottomBar({super.key, required this.onItemSelected}); // 👈 add it to constructor
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('BeLinker'),
-          ),
-
-          bottomNavigationBar: BottomNavigationBar(
-
-            selectedItemColor: Colors.black,     // اللون للآيكون/النص لما يكون مختار
-            unselectedItemColor: Colors.grey, // اللون للآيكونات/النصوص الغير مختارة
-            selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
-            type: BottomNavigationBarType.fixed,
-            items: const [
-              BottomNavigationBarItem(
-                  label: 'Explore',
-                  icon: Icon(Icons.search)
-
-              ),BottomNavigationBarItem(
-                  label: 'Review',
-                  icon: Icon(Icons.star)
-              ),BottomNavigationBarItem(
-                  label: 'Deals',
-                  icon: Icon(Icons.handshake)
-              ),BottomNavigationBarItem(
-                  label: 'Inbox',
-                  icon: Icon(Icons.chat)
-              ),
-              BottomNavigationBarItem(
-                  label: 'Profile',
-                  icon: Icon(Icons.account_circle))
-            ],
-          ) ,
-        )
+    return CurvedNavigationBar(
+      index: currentIndex,
+      height: 60,
+      color: Colors.orange,
+      backgroundColor: Colors.white,
+      onTap: (index) {
+        setState(() {
+          currentIndex = index;
+        });
+        widget.onItemSelected(index); // 👈 notify HomePage
+      },
+      items: const [
+        Icon(Icons.search, size: 30, color: Colors.white),
+        Icon(Icons.star, size: 30, color: Colors.white),
+        Icon(Icons.handshake, size: 30, color: Colors.white),
+        Icon(Icons.chat, size: 30, color: Colors.white),
+        Icon(Icons.account_circle, size: 30, color: Colors.white),
+      ],
     );
   }
 }
