a86bc04a3ab7cd419254df2517105724c5e9d84f
Anas Akkari <anasakkari04@gmail.com>
2025-09-24 14:52:45 +0300

    24/9

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,71 @@
 import 'package:flutter/material.dart';
-import 'Top_Bar.dart';
-import 'bottom_bar.dart';
+import 'BottomBar.dart';   // ✅ CustomSliverTabBar
+import 'Header.dart';
+import 'custom_tabbar.dart';
 
-void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
+
+void main() => runApp(const MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: HomePage(),
+));
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'BeLinker',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomePage(), // هنا تبدأ بالصفحة الرئيسية
-    );
-  }
+  State<HomePage> createState() => _HomePageState();
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  int _bottomIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this); // عندك 3 Tabs بالكود المعدل
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const Top_Bar(),
-      body: const Center(
-        child: Text("محتوى الصفحة الرئيسية"),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: const CustomAppBar(), // ✅ الهيدر مع اللوجو
+
+        body: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              CustomSliverTabBar(controller: _tabController),
+            ];
+          },
+          body: TabBarView(
+            controller: _tabController,
+            children: const [
+              Center(child: Text("request page")),
+              Center(child: Text("service page")),
+              Center(child: Text("suggestions page")),
+            ],
+          ),
+        ),
+
+        // ✅ البار السفلي
+        bottomNavigationBar: BottomBar(
+          onItemSelected: (index) {
+            setState(() {
+              _bottomIndex = index;
+            });
+          },
+        ),
       ),
-      bottomNavigationBar:  BottomBar(),
     );
   }
 }
